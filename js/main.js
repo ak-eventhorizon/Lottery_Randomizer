975db4303d2045cb3ca71b4fafa8d8dc8c3a6a93
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-12-13 12:46:47 +0300

    func generate UI 4 of 20 added
    function generateUi4of20At added

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ import * as model from "./modules/model.js";
 import * as view from "./modules/view.js";
 import * as controller from "./modules/controller.js";
 
-model.test();
-view.test();
-controller.test();
+view.generateUi4of20At('app');
 
 // Model - предоставляет данные и реагирует на команды контроллера, изменяя своё состояние
 
--- js/modules/view.js
+++ js/modules/view.js
@@ -1,8 +1,37 @@
 // @ts-check -- enable type checking
 'use strict';
 
-function test(){
-    console.log(`View check complete!`);
+function generateUi4of20At(rootID){
+
+    let appRootElement = document.getElementById(rootID);
+
+    let elementSet1 = document.createElement('div');
+    let elementSet2 = document.createElement('div');
+    let elementMainButton = document.createElement('button');
+    elementSet1.id = 'set1';
+    elementSet2.id = 'set2';
+    elementMainButton.id = 'mainButton';
+
+    for (let i = 1; i <= 20; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerHTML = `${i}`;
+        cell.id = `c1_${i}`;
+        elementSet1.appendChild(cell);
+    }
+
+    for (let i = 1; i <= 20; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerHTML = `${i}`;
+        cell.id = `c2_${i}`;
+        elementSet2.appendChild(cell);
+    }
+
+    appRootElement.appendChild(elementSet1);
+    appRootElement.appendChild(elementSet2);
+    appRootElement.appendChild(elementMainButton);
 }
 
-export { test };
+
+export { generateUi4of20At};
